5766e531cb8f6a2bbc34b9b955d54a53d7d05128
ftedhuijsko <hsjcelmuthmatos@hotmail.com>
2024-05-16 23:36:43 +0000

    feat project commit

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-function connect(root) {
-  if (!root) return root;
-  let levelStart = root;
-  while (levelStart) {
-    let curr = levelStart;
-    while (curr) {
-      if (curr.left) curr.left.next = curr.right;
-      if (curr.right && curr.next) curr.right.next = curr.next.left;
-      curr = curr.next;
-    }
-    levelStart = levelStart.left;
-  }
-  return root;
-}
+const last = (arr) => arr[arr.length - 1];
